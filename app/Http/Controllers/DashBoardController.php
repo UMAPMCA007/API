a02dd975463ef09ad0840e504455eed8b9934b1d
Midhun Chacko <thomas.midhun1@gmail.com>
2022-04-24 00:26:17 +0530

    Major bug fix

--- app/Http/Controllers/DashBoardController.php
+++ app/Http/Controllers/DashBoardController.php
@@ -49,16 +49,19 @@ class DashBoardController extends Controller
     {
         $activity=Activity::all();
         if($activity){
-            $last_add=Activity::all()->last()->pluck('created_at');
+            $last_add=Activity::all()->pluck('created_at')->last();
             // current date
             $current_date=date('Y-m-d');
             // last add date
             $last_add_date=date('Y-m-d',strtotime($last_add));
-            // if last add date is grater than current date
+            // if current date is not equal to last add date
             $activity=Activity::where('user_id',$id)->get();
-            if($last_add_date<$current_date)
+            $count=count($activity);
+            if($last_add_date!=$current_date)
             {
-                if(count($activity)<12)
+                    $count+=2;
+                    
+                if(count($activity)!=$count)
                 {
                     $activity=new Activity();
                     $activity->user_id=$id;
@@ -70,19 +73,31 @@ class DashBoardController extends Controller
                 }
                 else
                 {
-                    return response()->json(['error'=>'You have already added 2 activity']);
+                    return response()->json(['error'=>'You have already added 2 activity in one day']);
                 }
-            }else{
-                return response()->json(['error'=>'You have already added 2 activity in a day']);
-            }   
+            }else {
+                if(count($activity)<12)
+                {
+                        $activity=new Activity();
+                        $activity->user_id=$id;
+                        $activity->activity_type=$request->activity_type;
+                        $activity->activity_name=$request->activity_name;
+                        $activity->save();
+                        return response()->json(['success'=>'activity added successfully']);
+                    
+                }else{
+                    return response()->json(['error'=>'You have already added 2 activity in one day']);
+                }
+            }
             
+        
         }else{
             $activity=new Activity();
             $activity->user_id=$id;
             $activity->activity_type=$request->activity_type;
             $activity->activity_name=$request->activity_name;
             $activity->save();
-            return response()->json(['activity'=>$activity]);
+            return response()->json(['success'=>'activity added successfully']);
         }
         
     }
